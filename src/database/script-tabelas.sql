c93ae6e3058a59f0e16e5d3ac7f4f30962fe36d0
SoaresDev03 <alexandre.doliveira@sptech.school>
2025-11-27 02:11:10 -0300

    Remove sample user insertion from script-tabelas.sql
    Removed sample user insertion from usuario table.

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -21,8 +21,6 @@ perfil VARCHAR(20),
 		CHECK (perfil IN ('Conservador', 'Moderado', 'Arrojado'))
 );
 
-INSERT INTO usuario (nome, dtNascimento, email, senha, perfil) VALUES
-('Alexandre Silva', '1990-05-15', 'alexandre@example.com', 'senha123', 'Arrojado');
 
 CREATE TABLE empresa (
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
