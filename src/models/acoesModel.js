f4739ee2ef455b83689e2834fc0dd7735a98dd34
AnaMioki <ana.mioki@sptech.school>
2025-12-07 10:06:17 -0300

    adiciona rota responsavel por trazer todos os setores e adiciona no filtro do dropdown para ficar dinamico

--- src/models/acoesModel.js
+++ src/models/acoesModel.js
@@ -14,15 +14,15 @@ function listarTodasAcoesDeAcordoComPerfil() {
 
     --  EBITDA da ação
     ( it.valorMercado / it.multiploSetorial ) AS ebitda,
-     it.volume
+     it.volume,
 
-    -- it.ano -- n tenho crtza
+    it.ano -- n tenho crtza
         FROM empresa e
         JOIN infoTemporal it ON e.idEmpresa = it.fkEmpresa
         JOIN sub_acoes_calculado sub ON sub.fkEmpresa = e.idEmpresa;
-        -- se existirem vários anos, vão vir vários registros repetidos da mesma empresa.
-        -- isso só funciona se todas as empresas tiverem o mesmo conjunto de anos se não, pode “sumir” empresa.
-        -- WHERE it.ano = (SELECT MAX(ano) FROM infoTemporal);
+        -- garante que vai vir a ação mais recente
+        -- isso só funciona se todas as empresas tiverem o mesmo conjunto de anos se não, pode “sumir” empresa
+        WHERE it.ano = (SELECT MAX(ano) FROM infoTemporal);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
