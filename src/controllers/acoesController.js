7e2fdea881250c7c11d8bded58c2e6377f9604b2
AnaMioki <ana.mioki@sptech.school>
2025-12-08 15:11:27 -0300

    refatorei uma pa de coisa

--- src/controllers/acoesController.js
+++ src/controllers/acoesController.js
@@ -2,38 +2,39 @@ var acoesModel = require('../models/acoesModel');
 
 function listarTodasAcoesDeAcordoComPerfil(req, res) {
 
-    const perfil = req.params.perfil.toLowerCase();  //pede o perfil coomo requisição para puxar as ações corretas
-    
+    const perfil = req.params.perfil;  //pede o perfil coomo requisição para puxar as ações corretas
+    console.log("perfil recebido na rota: ", perfil);
+
     //acoesModel.listarTodasAcoesDeAcordoComPerfil(perfil)
-    acoesModel.listarTodasAcoesDeAcordoComPerfil()
+    acoesModel.listarTodasAcoesDeAcordoComPerfil(perfil)
+        .then(resultado => {
+        res.json(resultado)
+        // const filtradas = resultado.filter(acao => {
+        //     const vol = acao.volatilidade_media_ano;
+        //     const pvpa = acao.precoSobreValorPatrimonial;
+        //     const retorno = acao.rentabilidadeAnual;
 
-        .then( resultado => {
+        //     if (perfil === "conservador") {
+        //         return vol < 0.5 && pvpa <= 1;
+        //     }
+        //     if (perfil === "moderado") {
+        //         return vol < 1.5 && retorno >= 1 && retorno <= 20;
+        //     }
+        //     if (perfil === "arrojado") {
+        //         return vol >= 0.5 && vol <= 4 && retorno > 5;
+        //     }
 
-            const filtradas = resultado.filter(acao => {
-                const vol = acao.volatilidade_media_ano;
-                const pvpa = acao.precoSobreValorPatrimonial;
-                const retorno = acao.rentabilidadeAnual;
-
-                 if (perfil === "conservador") {
-                    return vol < 0.5 && pvpa <= 1;
-                }
-                if (perfil === "moderado") {
-                    return vol < 1.5 && retorno >= 1 && retorno <= 20;
-                }
-                if (perfil === "arrojado") {
-                    return vol >= 0.5 && vol <= 4 && retorno > 5;
-                }
-
-                return false; // se nao estier em nenhum
-            });
-
-            res.json(filtradas);
-        })
+        //     return false; // se nao estier em nenhum
+        // });
+        // console.log("Ações filtradas: ", filtradas);
+        // res.json(filtradas);
+    })
         .catch(erro => {
-             console.log("erro ao listar ações: ", erro.sqlMessage);
+            console.log("erro ao listar ações: ", erro.sqlMessage);
             res.status(500).json(erro.sqlMessage);
         });
 }
+
 
 function listarSetores(req, res) {
     acoesModel.listarSetores()
@@ -50,31 +51,31 @@ function listarAcoesPorSetor(req, res) {
     const perfil = req.params.perfil.toLowerCase();
     var setor = req.params.setor;
 
-    setoresModel.listarAcoesPorSetor(setor).then(resultado => {
+    acoesModel.listarAcoesPorSetor(setor).then(resultado => {
 
-            let filtradas = resultado;
+        let filtradas = resultado;
 
-            // if (setor !== "Todos") {
-            //     filtradas = filtradas.filter(a => a.setor === setor);
-            // }
+        // if (setor !== "Todos") {
+        //     filtradas = filtradas.filter(a => a.setor === setor);
+        // }
 
-            filtradas = filtradas.filter(acao => {
-                const vol = acao.volatilidade_media_ano;
-                const pvpa = acao.precoSobreValorPatrimonial;
-                const retorno = acao.rentabilidadeAnual;
+        filtradas = filtradas.filter(acao => {
+            const vol = acao.volatilidade_media_ano;
+            const pvpa = acao.precoSobreValorPatrimonial;
+            const retorno = acao.rentabilidadeAnual;
 
-                if (perfil === "conservador") {
-                    return vol < 0.5 && pvpa <= 1;
-                }
-                if (perfil === "moderado") {
-                    return vol < 1.5 && retorno >= 1 && retorno <= 20;
-                }
-                if (perfil === "arrojado") {
-                    return vol >= 0.5 && vol <= 4 && retorno > 5;
-                }
+            if (perfil === "conservador") {
+                return vol < 0.5 && pvpa <= 1;
+            }
+            if (perfil === "moderado") {
+                return vol < 1.5 && retorno >= 1 && retorno <= 20;
+            }
+            if (perfil === "arrojado") {
+                return vol >= 0.5 && vol <= 4 && retorno > 5;
+            }
 
-                return false;
-            });
+            return false;
+        });
         // } else {
         //     res.status(204).send("Nenhuma ação encontrada para este setor!");
         // }
